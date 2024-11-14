d0c718d39607090a48abd2509d03014ffa25671d
Danlee García <soydanleegarcia@gmail.com>
2024-11-14 00:36:53 -0300

    trate de hacer  un login per me tare

---
+++ ProyectoProgramacion2/Login.aspx
@@ -0,0 +1,46 @@
+﻿<button type="submit" class="btn btn-primary mb-3">Confirm identity</button><%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ProyectoProgramacion2.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="Login.css" />
+    <title></title>
+</head>
+<body>
+    <div class="container mt-4">
+        <h2 class="text-primary mb-4">Ingreso Tecnico</h2>
+
+
+
+        <div class="form-group row">
+            <label for="txtCI" class="col-sm-3 col-form-label font-weight-bold">Cédula de Identidad:</label>
+            <div class="col-sm-9 d-flex align-items-center">
+                <asp:TextBox ID="txtCI" TextMode="Number" runat="server" CssClass="form-control" placeholder="Ingrese su cédula"></asp:TextBox>
+                <span class="fst-italic">(*)</span>
+                <asp:RequiredFieldValidator runat="server" ID="rfvCI" ValidationGroup="formRequerido" ControlToValidate="txtCI" CssClass="text-danger small" Text="El número de documento es requerido."></asp:RequiredFieldValidator>
+            </div>
+        </div>
+
+
+
+
+        <br />
+        <div class="form-group row">
+            <div class="col-sm-9 offset-sm-3">
+                <asp:Button ID="btnLogIn" runat="server" Text="Log-In" CausesValidation="true" ValidationGroup="formRequerido" CssClass="btn btn-primary" OnClick="cmdbtnLogIn_Click" />
+            </div>
+            <div class="col-sm-9 offset-sm-3">
+                <asp:Button ID="btnAdmin" runat="server" Text="Admin"  CssClass="btn" OnClick="cmdbtnAdmin_Click" />
+            </div>
+        </div>
+        
+    </div>
+
+
+
+    <asp:Label ID="lblError" runat="server" Visible="false" CssClass="text-danger"></asp:Label>
+    </div>
+</body>
+</html>
---
+++ ProyectoProgramacion2/Login.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ProyectoProgramacion2
+{
+
+
+    public partial class Login
+    {
+
+        /// <summary>
+        /// Control txtCI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCI;
+
+        /// <summary>
+        /// Control rfvCI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvCI;
+
+        /// <summary>
+        /// Control btnLogIn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogIn;
+
+        /// <summary>
+        /// Control btnAdmin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAdmin;
+
+        /// <summary>
+        /// Control lblError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+    }
+}
